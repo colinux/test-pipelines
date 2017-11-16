5f429e97b4ce87d5cb0e3e51b04277b2dd14a7fe
Colin Darie <colin@darie.eu>
2017-11-16 12:16:35 +0100

    new feature

--- app.rb
+++ app.rb
@@ -2,5 +2,6 @@ require 'dotenv/load'
 require 'sinatra'
 
 get '/' do
-  "<p>Hello world! I'm running with <strong>#{ENV.fetch('APP_ENV')}</strong> app env."
+  "<p>Hello Heroku! I'm running with <strong>#{ENV.fetch('APP_ENV')}</strong> app env,
+  and PUBLIC_SET_VALUE=#{ENV.fetch('PUBLIC_SET_VALUE')}, RACK=#{ENV.fetch('RACK_ENV')}"
 end
