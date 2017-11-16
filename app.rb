205ec17f5e766b2581d12d70b7aaba9031a2f6ff
Colin Darie <colin@darie.eu>
2017-11-16 12:55:37 +0100

    Another test

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'dotenv/load'
 require 'sinatra'
 
 get '/' do
-  "<p>Hello Heroku! I'm running with <strong>#{ENV.fetch('APP_ENV')}</strong> app env,
-  and PUBLIC_SET_VALUE=#{ENV.fetch('PUBLIC_SET_VALUE')}, RACK=#{ENV.fetch('RACK_ENV')}"
+  "<p>Hello Heroku, it's another feature! I'm running with <strong>#{ENV.fetch('APP_ENV')}</strong> app env,
+  and PUBLIC_REVIEW_APP_ONLY_VALUE=#{ENV.fetch('PUBLIC_REVIEW_APP_ONLY_VALUE')}, RACK=#{ENV.fetch('RACK_ENV')}"
 end
